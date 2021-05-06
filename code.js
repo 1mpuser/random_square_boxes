26e72e739bb3e39ccb3af12396637946deb5ecce
1mpuser <alex022003@mail.ru>
2021-05-06 21:09:40 +0300

    trash commit

---
+++ code.js
@@ -0,0 +1,7 @@
+let game=document.getElementById('game');
+game.style.visibility='hidden';
+let button=document.querySelector('button');
+function click(){
+    if (game.style.visibility=='hidden') game.style.visibility='			width: 50px;height: 50px;line-height: 50px;text-align: center;font-family: arial;display: inline-block;font-size: 26px;border: 1px solid #000;cursor: pointer;vertical-align: middle;transition: background .1s;';
+    else game.style.visibility='hidden';
+}
