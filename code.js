7856228fcf315afbc3e7c38a8f012ae3f57a7954
1mpuser <alex022003@mail.ru>
2021-05-08 01:23:35 +0300

    first normal commit

--- code.js
+++ code.js
@@ -1,7 +1,98 @@
+"use strict"
 let game=document.getElementById('game');
-game.style.visibility='hidden';
-let button=document.querySelector('button');
+let cells=document.getElementsByClassName('cell');
+let start=document.getElementById('start');
+start.addEventListener('click',getStarted)
+let arrOfColours=['maroon', 'chocolate', 'navy','aquamarine' ,'lime', 'fuchsia', 'teal', 'khaki'];
+let arrOfSizes=[30, 40,26,20,12,35,15];
+let counter=1;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  }
+
+function getStarted(){
+
+let arrOfDataId=[];
+let firstArr=[];
+let secondArr=[];
+for (let i=0;i<=24;i++) {arrOfDataId[i]=i;}
+for (let i=1;i<=25;i++){
+let randomPickedIndex=getRandomInt(0,arrOfDataId.length);
+fillingCellsWithNumbers(arrOfDataId[randomPickedIndex],i);
+secondArr=arrOfDataId.slice(randomPickedIndex+1);
+firstArr=arrOfDataId.slice(0,randomPickedIndex);
+arrOfDataId=[];
+arrOfDataId=firstArr.concat(secondArr);
+randomCellsFill();
+
+}}
+
+
+function fillingCellsWithNumbers(index,number) {
+    for (let i=0;i<cells.length;i++) {
+        if (cells[i].getAttribute('data-id') == index) {
+            cells[i].innerText=number;}}
+}
+
+function findMaxNumberInArray(arr){
+    let max=arr[0];
+    for (let i=0;i<arr.length;i++) if (arr[i]>max) max=arr[i]
+    return +max;
+}
+
+
+function findMinNumberInArray(arr){
+    let min=arr[0];
+    for (let i=0;i<arr.length;i++) if (arr[i]<min) min=arr[i];
+    return +min;
+}
+
+function returnRandomShitFromArray(arr){
+    let index=getRandomInt(0,arr.length);
+    return arr[index];
+}
+
+
+function randomCellsFill(){
+    for (let i=0;i<cells.length;i++){
+        cells[i].style.fontSize =returnRandomShitFromArray(arrOfSizes) +'px';
+        cells[i].style.color=returnRandomShitFromArray(arrOfColours);
+        cells[i].addEventListener('click', getPicked);
+    }
+}
+
+
+function getPicked(){
+if (this.innerText==counter) {
+    this.style.background='red';
+    counter++;
+    if (counter>25) win();}}
+
+
+
+
+
+function timer(){
+
+}
+
+function lose(){
+
+}
+function win(){
+
+}
+
+
+function restart(){
+
+}
+
+
+
 function click(){
-    if (game.style.visibility=='hidden') game.style.visibility='			width: 50px;height: 50px;line-height: 50px;text-align: center;font-family: arial;display: inline-block;font-size: 26px;border: 1px solid #000;cursor: pointer;vertical-align: middle;transition: background .1s;';
-    else game.style.visibility='hidden';
+    game.style.visibility='visible';
 }
