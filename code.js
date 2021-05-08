8925c6456130196db9de6f423f47013cee99de86
1mpuser <alex022003@mail.ru>
2021-05-08 13:06:04 +0300

    second commit

--- code.js
+++ code.js
@@ -2,7 +2,10 @@
 let game=document.getElementById('game');
 let cells=document.getElementsByClassName('cell');
 let start=document.getElementById('start');
-start.addEventListener('click',getStarted)
+let restart=document.getElementById('restart');
+let p=document.querySelector('p');
+start.addEventListener('click',getStarted);
+restart.addEventListener('click', restartGame);
 let arrOfColours=['maroon', 'chocolate', 'navy','aquamarine' ,'lime', 'fuchsia', 'teal', 'khaki'];
 let arrOfSizes=[30, 40,26,20,12,35,15];
 let counter=1;
@@ -14,7 +17,11 @@ function getRandomInt(min, max) {
   }
 
 function getStarted(){
-
+restart.style.visibility='visible';
+restart.disabled=false;
+game.style.visibility='visible';
+this.style.visibility='hidden';
+this.disabled=true;
 let arrOfDataId=[];
 let firstArr=[];
 let secondArr=[];
@@ -27,7 +34,7 @@ firstArr=arrOfDataId.slice(0,randomPickedIndex);
 arrOfDataId=[];
 arrOfDataId=firstArr.concat(secondArr);
 randomCellsFill();
-
+timer();
 }}
 
 
@@ -67,30 +74,70 @@ function randomCellsFill(){
 
 function getPicked(){
 if (this.innerText==counter) {
-    this.style.background='red';
+    this.style.background='maroon';
     counter++;
     if (counter>25) win();}}
 
 
 
-
-
+let secondsRemain;
+let isOffTimerId=true;
 function timer(){
+secondsRemain=76;
+time();
+if(isOffTimerId) window.timerId=window.setInterval(time,1000);
+window.setInterval(function() {
+if (checkWin()) window.clearInterval(window.timerId);
+},1);
+isOffTimerId=false;
 
 }
 
 function lose(){
-
+p.innerText='Вы проиграли';
+for (let i=0;i< cells.length;i++) cells[i].removeEventListener('click',getPicked);
 }
 function win(){
+p.innerText='Вы победили';
+isOffTimerId=true;
 
 }
 
 
-function restart(){
+function restartGame(){
+for (let i=0;i<cells.length;i++){
+    cells[i].innerText='';
+    cells[i].style.background='';
+    cells[i].removeEventListener('click',getPicked);
+    let arrOfDataId=[];
+let firstArr=[];
+let secondArr=[];
+for (let i=0;i<=24;i++) {arrOfDataId[i]=i;}
+for (let i=1;i<=25;i++){
+let randomPickedIndex=getRandomInt(0,arrOfDataId.length);
+fillingCellsWithNumbers(arrOfDataId[randomPickedIndex],i);
+secondArr=arrOfDataId.slice(randomPickedIndex+1);
+firstArr=arrOfDataId.slice(0,randomPickedIndex);
+arrOfDataId=[];
+arrOfDataId=firstArr.concat(secondArr);
+randomCellsFill();
+counter=1;
+timer();
+}
+}}
 
+function checkWin(){
+    if (counter>25) return true;
 }
 
+
+function time(){
+    secondsRemain--;
+    if (secondsRemain<0) return lose();
+    let str;
+    if (secondsRemain<10) str='0'+secondsRemain;
+    p.innerText='Времени осталось : '+ secondsRemain;
+}
 
 
 function click(){
